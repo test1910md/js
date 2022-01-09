370bec405d6889f45fbbf95686b39a27aceb68ea
mari <m.dorokhova@pnlfin.tech>
2022-01-09 12:45:20 +0500

    secondhw

---
+++ drop/app.js
@@ -0,0 +1,50 @@
+const item =document.querySelector('.item')
+console.log(item)
+
+const placeholders = document.querySelectorAll('.placeholder')
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+}
+
+item.addEventListener("dragstart", dragstart)
+item.addEventListener("dragend", dragend)
+
+function dragstart(event) {
+    console.log('drag start', event.target)
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add('hide'), 0)
+    
+}
+
+function dragend(event) {
+    console.log('drag end')
+    event.target.classList.remove('hold', 'hide')
+    // event.target.className = 'item'
+
+}
+
+function dragover(event) {
+    event.preventDefault()
+    
+
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+
+}
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+
+}
